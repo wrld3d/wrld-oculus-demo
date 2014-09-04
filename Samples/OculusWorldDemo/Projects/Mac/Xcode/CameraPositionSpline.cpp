de1d28178a2895be5c87f9e3be877c189d0891ec
Salvador Lopez <salvador.lopez@eegeo.com>
2014-09-04 14:29:16 +0100

    Print spline points when presssing K. Sample spline loaded by default.

--- Samples/OculusWorldDemo/Projects/Mac/Xcode/CameraPositionSpline.cpp
+++ Samples/OculusWorldDemo/Projects/Mac/Xcode/CameraPositionSpline.cpp
@@ -19,6 +19,25 @@ namespace Eegeo
             m_forwardSpline.AddPoint(dv3(forward.x, forward.y, forward.z));
         }
         
+        void CameraPositionSpline::Spew()
+        {
+            const std::vector<Geometry::CatmullRomSplinePoint>& rightPoints = m_rightSpline.GetPoints();
+            const std::vector<Geometry::CatmullRomSplinePoint>& upPoints = m_upSpline.GetPoints();
+            const std::vector<Geometry::CatmullRomSplinePoint>& forwardPoints = m_forwardSpline.GetPoints();
+            const std::vector<Geometry::CatmullRomSplinePoint>& positionPoints = m_positionSpline.GetPoints();
+            printf("m33 orientation;\n");
+            
+            for(int i =0; i< rightPoints.size();i++)
+            {
+                const dv3 right = rightPoints.at(i).Position;
+                const dv3 up = upPoints.at(i).Position;
+                const dv3 forward = forwardPoints.at(i).Position;
+                const dv3 position = positionPoints.at(i).Position;
+                printf("orientation.SetFromBasis(Eegeo::v3(%f,%f,%f),Eegeo::v3(%f,%f,%f),Eegeo::v3(%f,%f,%f));\n", right.x,right.y,right.z,up.x,up.y,up.z,forward.x,forward.y,forward.z);
+                printf("AddPoint(Eegeo::dv3(%f,%f,%f), orientation);\n",position.x,position.y,position.z);
+            }
+        }
+        
         void CameraPositionSpline::Start()
         {
             m_time = 0.0;
@@ -53,9 +72,9 @@ namespace Eegeo
             m_forwardSpline.GetInterpolatedPositionInPlace(m_time, forward);
             m_upSpline.GetInterpolatedPositionInPlace(m_time, up);
             m_rightSpline.GetInterpolatedPositionInPlace(m_time, right);
-            interpolatedOrientation.SetRow(0, right.ToSingle());
-            interpolatedOrientation.SetRow(1, up.ToSingle());
-            interpolatedOrientation.SetRow(2, forward.ToSingle());
+            interpolatedOrientation.SetRow(0, right.Norm().ToSingle());
+            interpolatedOrientation.SetRow(1, up.Norm().ToSingle());
+            interpolatedOrientation.SetRow(2, forward.Norm().ToSingle());
         }
     }
 }
--- Samples/OculusWorldDemo/Projects/Mac/Xcode/CameraPositionSpline.h
+++ Samples/OculusWorldDemo/Projects/Mac/Xcode/CameraPositionSpline.h
@@ -18,6 +18,25 @@ namespace Eegeo
             , m_playing(false)
             , m_time(0.0)
             {
+                m33 orientation;
+                orientation.SetFromBasis(Eegeo::v3(-0.181377,-0.110160,0.977223),Eegeo::v3(0.728775,0.652148,0.208780),Eegeo::v3(0.660294,-0.750046,0.038002));
+                AddPoint(Eegeo::dv3(4648172.292439,4159580.242028,1331625.804022), orientation);
+                orientation.SetFromBasis(Eegeo::v3(0.061158,-0.365653,0.928737),Eegeo::v3(0.728776,0.652152,0.208768),Eegeo::v3(0.682016,-0.664078,-0.306365));
+                AddPoint(Eegeo::dv3(4647918.212886,4159788.902805,1331710.577593), orientation);
+                orientation.SetFromBasis(Eegeo::v3(-0.221746,-0.063747,0.973017),Eegeo::v3(0.728776,0.652142,0.208809),Eegeo::v3(0.647856,-0.755416,0.098153));
+                AddPoint(Eegeo::dv3(4647929.349371,4160099.512685,1331821.951101), orientation);
+                orientation.SetFromBasis(Eegeo::v3(-0.004384,-0.300435,0.953790),Eegeo::v3(0.728779,0.652151,0.208770),Eegeo::v3(0.684737,-0.696021,-0.216091));
+                AddPoint(Eegeo::dv3(4647868.438146,4160170.536716,1331812.722798), orientation);
+                orientation.SetFromBasis(Eegeo::v3(-0.643567,0.756417,-0.116844),Eegeo::v3(0.728666,0.652228,0.208925),Eegeo::v3(-0.234246,-0.049319,0.970928));
+                AddPoint(Eegeo::dv3(4647672.783374,4160141.472382,1331674.231118), orientation);
+                orientation.SetFromBasis(Eegeo::v3(-0.416373,0.664006,-0.621070),Eegeo::v3(0.728615,0.652290,0.208909),Eegeo::v3(-0.543837,0.365536,0.755400));
+                AddPoint(Eegeo::dv3(4647572.540046,4160282.525131,1331583.460096), orientation);
+                orientation.SetFromBasis(Eegeo::v3(-0.170106,0.467721,-0.867352),Eegeo::v3(0.728587,0.652333,0.208879),Eegeo::v3(-0.663500,0.596408,0.451739));
+                AddPoint(Eegeo::dv3(4647853.716026,4160544.688893,1331627.942732), orientation);
+                orientation.SetFromBasis(Eegeo::v3(0.552680,-0.379914,-0.741760),Eegeo::v3(0.728579,0.652396,0.208715),Eegeo::v3(-0.404627,0.655780,-0.637363));
+                AddPoint(Eegeo::dv3(4647935.263351,4160641.274088,1331610.706218), orientation);
+                orientation.SetFromBasis(Eegeo::v3(-0.077166,-0.224347,0.971445),Eegeo::v3(0.728930,0.652075,0.208491),Eegeo::v3(0.680228,-0.724208,-0.113214));
+                AddPoint(Eegeo::dv3(4648349.368964,4159360.308238,1331560.434232), orientation);
             }
             
             void AddPoint(const dv3& ecefEyePosition, const m33& orientation);
@@ -30,6 +49,8 @@ namespace Eegeo
             void SetPlaybackSpeed(double speed) { m_playbackSpeed = speed; }
             
             const bool IsPlaying() const { return m_playing; }
+            
+            void Spew();
             
             void GetCurrentCameraPosition(dv3& interpolatedPositionEcef, m33& interpolatedOrientation) const;
         private:
